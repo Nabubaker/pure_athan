717e78394cccf0af0d13bf9f9fcf1615a514bc5b
sami <shassen95@gmail.com>
2018-10-01 21:21:31 -0400

    Create notification method activity and fragment

---
+++ app/src/main/java/com/pure/pureathan/NotificationListFragment.java
@@ -0,0 +1,44 @@
+/*
+    Author: Sami Ibrahim
+    Email: shassen95@gmail.com
+ */
+
+package com.pure.pureathan;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_notification_list, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.notification_list_recycler_view);
+
+        /* Changes in content don't change the layout size of the RecyclerView */
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        //String[] notificationMethods = {"Mecca", "Medina", "Egypt", "Single Beep", "Double Beep", "Triple Beep"};
+        String[] notificationMethods = {};
+
+        mAdapter = new NotificationListAdapter(notificationMethods);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/pure/pureathan/NotificationMethodActivity.java
@@ -0,0 +1,15 @@
+package com.pure.pureathan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class NotificationMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_method);
+    }
+}
